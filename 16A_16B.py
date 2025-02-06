afda3679b63f7985f5bae533e8f75c3fbfa2af2d
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2025-02-06 11:00:14 -0100

    Update 16A_16B.py

--- 16A_16B.py
+++ 16A_16B.py
@@ -1,1 +1,42 @@
+#Question 16(a)
+#name = imogene murphy and School = meán scoil Nua An Leith Triúigh
 
+data_list=[23,57,38,45,91,38,76,65,54,31,77,38,13,88,47,20,13,68]
+#(iv)
+# #(i)
+# data_list.sort()
+# print("the sorted list is: ",data_list)
+# 
+# #(ii)
+# numInList = 0
+# counter= 0
+# for i in data_list:
+#     counter=counter + i
+#     numInList= numInList + 1
+# mean= counter/numInList
+# print(mean)
+# 
+# #(iii)
+# last = data_list[9]
+# first = data_list[8]   
+# def find_median(data_list):
+#     mid = last+first
+#     mid =mid/2
+#     print(mid)
+#     return(find_median)
+# find_median(find_median)
+#(v)    
+def stem_and_leaf_plot(data_list):
+
+#Question 16(b)
+#Name= imogene murphy and School = meán scoil nua an leigth TriUigh
+
+
+def hash_function(input_string):
+    normalMessage =(input("please enter any message: "))
+    
+    
+    print("the hash_value of",normalMessage,"is:")
+    return(hash_function)
+
+hash_function(hash_function)
